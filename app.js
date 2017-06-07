5fc052419cde0ee6c2378bdbd6de6d8911ad97d6
Richard Saville <richardasaville@gmail.com>
2017-06-07 17:58:08 +1000

    added cors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.use(cors());
 
 const mlabpassword = process.env.WASHYPASSWORD
 var mongoose = require('mongoose');
